b0659581781868b0a12bc2ed0024c1a35e9b8833
transcendenztime <46561690+transcendenztime@users.noreply.github.com>
2019-03-28 13:47:38 +0300

    add "processList" function
    Transfer logics from "render" function to "processList".

--- filter/Filter.js
+++ filter/Filter.js
@@ -16,36 +16,61 @@ var Filter = React.createClass({
           searchText: "",
           isSorted: false,
           //searchText: this.props.defaultSearchText,
+		  dictionaryState: this.props.dictionary,
         };
     },
 
     searchTextChanged: function(EO) {
-        this.setState({searchText:EO.target.value});
+		this.processList(EO.target.value, this.state.isSorted);
+		
+		//можно выполнить следующий код вместо вызова "processList"
+		/*var stringsForRender = this.props.dictionary.filter(oneString => oneString.indexOf(EO.target.value) !== -1);
+		
+		this.setState({dictionaryState : stringsForRender});
+		this.setState({searchText: EO.target.value});*/
     },
 
     sortAll: function(EO){
-        this.setState({isSorted:EO.target.checked});
+		this.processList(this.state.searchText, EO.target.checked);
+		
+		//можно выполнить следующий код вместо вызова "processList"
+		/*var stringsForRender = this.props.dictionary.filter(oneString => oneString.indexOf(this.state.searchText) !== -1);
+		
+		EO.target.checked
+			?stringsForRender.sort()
+			:null
+			
+		this.setState({isSorted:EO.target.checked});
+		this.setState({dictionaryState : stringsForRender});*/
     },
 
     clearAll: function() {
         this.setState({searchText: ""});
         this.setState({isSorted:false});
+		this.setState({dictionaryState: this.props.dictionary});
     },
 
+	processList: function(searchText, isSorted) {
+		var stringsForRender = this.props.dictionary.filter(oneString => oneString.indexOf(searchText) !== -1);
+
+		isSorted
+			?stringsForRender.sort()
+            :null
+		
+		this.setState({dictionaryState : stringsForRender});
+		this.setState({searchText:searchText});
+		this.setState({isSorted:isSorted});
+	},
+	
     render: function() {
 
         var allStrings = [];
         var strKey=0;
-
-        var stringsForRender = this.props.dictionary.filter(oneString => oneString.indexOf(this.state.searchText) !== -1);
-        //сортируем массив строк, если isSorted == true
-        this.state.isSorted
-            ?stringsForRender.sort()
-            :null
-
-        stringsForRender.forEach(function(str) {
+		//console.log(this.state.dictionaryState);
+        
+		this.state.dictionaryState.forEach(function(str) {
             var oneString = 
-              React.DOM.div({key:strKey,className:'FilterRow'}, str, );
+              React.DOM.div({key: strKey,className:'FilterRow'}, str, );
               allStrings.push(oneString);
               strKey++;
         }
